42451acbe040721ac03e4ccf603d1e55ea466d77
Diego Iza Viteri <diegoiza@hotmail.com>
2018-07-07 20:27:39 -0500

    solución prueba

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            testEntities te = new testEntities();
+            te.t1.ToList().ForEach(p => Console.Write(p.id.ToString(), p.c1));
+            Console.ReadKey();
+        }
+    }
+}
